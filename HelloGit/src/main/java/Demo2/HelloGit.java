c7cc64eac0c3ef20a0179f5d8ea6aff024521ddd
vip-01 <vip_010821@163.com>
2022-06-27 23:16:25 +0800

    添加文件

---
+++ HelloGit/src/main/java/Demo2/HelloGit.java
@@ -0,0 +1,14 @@
+package Demo2;
+
+/**
+ * @param
+ * @param
+ * @author shkstart
+ * @Description
+ * @create 2022-06-27 23:03
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
