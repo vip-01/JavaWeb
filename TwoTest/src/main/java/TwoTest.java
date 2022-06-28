00725e5e5065547e6bbda169cc5776a29c6efb07
vip-01 <vip_010821@163.com>
2022-06-28 23:32:02 +0800

    第二个git的测试代码文件

---
+++ TwoTest/src/main/java/TwoTest.java
@@ -0,0 +1,12 @@
+/**
+ * @param
+ * @param
+ * @author shkstart
+ * @Description
+ * @create 2022-06-28 23:28
+ */
+public class TwoTest {
+    public static void main(String[] args) {
+        System.out.println("Two Test!");
+    }
+}
