0c879f3ba760615322c00efc006d717f7e53528d
vip-01 <vip_010821@163.com>
2022-06-28 22:35:42 +0800

    关于git的测试

---
+++ HelloWork/src/main/java/Demo1/HelloWork.java
@@ -0,0 +1,14 @@
+package Demo1;
+
+/**
+ * @param
+ * @param
+ * @author shkstart
+ * @Description
+ * @create 2022-06-28 22:34
+ */
+public class HelloWork {
+    public static void main(String[] args) {
+        System.out.println("Hello Work!");
+    }
+}
