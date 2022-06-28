c64416998a0346971fd73889b53712284f6641e8
vip-01 <vip_010821@163.com>
2022-06-28 18:35:59 +0800

    git测试文件

---
+++ WorkGit/src/main/java/Demo4/HelloGit.java
@@ -0,0 +1,14 @@
+package Demo4;
+
+/**
+ * @param
+ * @param
+ * @author shkstart
+ * @Description
+ * @create 2022-06-28 18:31
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
